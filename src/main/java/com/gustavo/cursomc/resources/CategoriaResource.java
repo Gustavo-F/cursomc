b902dce705188e6e8f356eb0293607d854820fac
Gustavo-F <fgustavo016@gmail.com>
2022-02-17 09:22:19 -0300

    created repository and service to Categoria domain

--- src/main/java/com/gustavo/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/gustavo/cursomc/resources/CategoriaResource.java
@@ -1,27 +1,25 @@
 package com.gustavo.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gustavo.cursomc.domain.Categoria;
+import com.gustavo.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
-	@GetMapping
-	public List<Categoria> listar() {
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(1, "Escritório");
 
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> find(@PathVariable("id") Integer id) {
+		Categoria categoria = categoriaService.buscar(id);
+		return ResponseEntity.ok(categoria);
 	}
 }
