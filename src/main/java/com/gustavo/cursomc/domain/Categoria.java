9fff3b71908fa481b0735ec2e971d4d8f56f060a
Gustavo <fgustavo0@gmail.com>
2022-02-17 00:17:46 -0300

    added h2 database using jpa

--- src/main/java/com/gustavo/cursomc/domain/Categoria.java
+++ src/main/java/com/gustavo/cursomc/domain/Categoria.java
@@ -2,8 +2,16 @@ package com.gustavo.cursomc.domain;
 
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria {
-	private Integer id;
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+ 	private Integer id;
 	private String nome;
 	
 	public Categoria() {}
