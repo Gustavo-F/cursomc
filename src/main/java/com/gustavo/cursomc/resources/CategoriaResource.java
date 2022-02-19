986d09fa848f64fa05ebad7890112de68db0ee5e
Gustavo-F <fgustavo016@gmail.com>
2022-02-19 12:46:05 -0300

    created POST method to Categoria

--- src/main/java/com/gustavo/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/gustavo/cursomc/resources/CategoriaResource.java
@@ -4,8 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 import com.gustavo.cursomc.domain.Categoria;
 import com.gustavo.cursomc.services.CategoriaService;
@@ -21,5 +26,15 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable("id") Integer id) {
 		Categoria categoria = categoriaService.buscar(id);
 		return ResponseEntity.ok(categoria);
+	}
+
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Categoria categoria) {
+		categoria  = categoriaService.insert(categoria);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+			.path("/{id}").buildAndExpand(categoria.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
 	}
 }
