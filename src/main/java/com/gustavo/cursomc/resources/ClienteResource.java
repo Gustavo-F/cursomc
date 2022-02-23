54a1d5c15f6d01c66ee4cd48f3e758d62f697d56
Gustavo-F <fgustavo016@gmail.com>
2022-02-23 13:32:56 -0300

    fix CategoriaSerive update method and Pagamento constructor

--- src/main/java/com/gustavo/cursomc/resources/ClienteResource.java
+++ src/main/java/com/gustavo/cursomc/resources/ClienteResource.java
@@ -1,6 +1,5 @@
 package com.gustavo.cursomc.resources;
 
-import java.net.URI;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -15,11 +14,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
--- src/main/java/com/gustavo/cursomc/services/CategoriaService.java
+++ src/main/java/com/gustavo/cursomc/services/CategoriaService.java
@@ -40,9 +40,11 @@ public class CategoriaService {
 		return repo.save(categoria);
     }
 
-    public Categoria update(Categoria categoria) {
-        find(categoria.getId());
-		return repo.save(categoria);
+	public Categoria update(Categoria categoria) {
+        Categoria newCategoria = find(categoria.getId());
+		updateData(newCategoria, categoria);
+		
+		return repo.save(newCategoria);
     }
 
     public void delete(Integer id) {
@@ -64,6 +66,10 @@ public class CategoriaService {
     public Categoria fromDTO(CategoriaDTO categoriaDTO) {
     	return new Categoria(categoriaDTO.getId(), categoriaDTO.getNome());	
     }
+
+	private void updateData(Categoria newCategoria, Categoria categoria) {
+		newCategoria.setNome(categoria.getNome());
+	}
 }
 
 
