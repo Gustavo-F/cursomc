18a70a82fad62d814b5604f488faafdc6fe0b574
Gustavo Ferreira <fgustavo016@gmail.com>
2022-04-07 09:48:54 -0300

    restrict content from Pedido by user

--- src/main/java/com/gustavo/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/gustavo/cursomc/repositories/PedidoRepository.java
@@ -1,9 +1,15 @@
 package com.gustavo.cursomc.repositories;
 
+import com.gustavo.cursomc.domain.Cliente;
 import com.gustavo.cursomc.domain.Pedido;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
-    
+
+    @Transactional(readOnly = true)
+    public Page<Pedido> findByCliente(Cliente cliente, Pageable pageable);
 }
--- src/main/java/com/gustavo/cursomc/services/PedidoService.java
+++ src/main/java/com/gustavo/cursomc/services/PedidoService.java
@@ -3,6 +3,7 @@ package com.gustavo.cursomc.services;
 import java.util.Date;
 import java.util.Optional;
 
+import com.gustavo.cursomc.domain.Cliente;
 import com.gustavo.cursomc.domain.ItemPedido;
 import com.gustavo.cursomc.domain.PagamentoBoleto;
 import com.gustavo.cursomc.domain.Pedido;
@@ -10,9 +11,14 @@ import com.gustavo.cursomc.domain.enums.EstadoPagamento;
 import com.gustavo.cursomc.repositories.ItemPedidoRepository;
 import com.gustavo.cursomc.repositories.PagamentoRepository;
 import com.gustavo.cursomc.repositories.PedidoRepository;
+import com.gustavo.cursomc.security.UserSS;
+import com.gustavo.cursomc.services.exceptions.AuthorizationException;
 import com.gustavo.cursomc.services.exceptions.ObjectNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -74,5 +80,16 @@ public class PedidoService {
         emailService.sendOrderConfirmationHtmlEmail(pedido);
 
         return pedido;
+    }
+
+    public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        UserSS user = UserService.authenticated();
+        if (user == null)
+            throw new AuthorizationException("Acesso negado");
+
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+        Cliente cliente = clienteService.find(user.getId());
+
+        return repo.findByCliente(cliente, pageRequest);
     }
 }
